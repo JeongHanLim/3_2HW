5929bc0c5bca4fb0fe7ab9577687074f2f608487
JeongHanLim <jhlim271@naver.com>
2021-04-27 13:52:51 +0900

    Reinforcement learning add.

--- RL/Middle_Test/pacmanDQNAgents.py
+++ RL/Middle_Test/pacmanDQNAgents.py
@@ -54,7 +54,7 @@ class DQN(torch.nn.Module):
 
 class ReplayMemory:
     def __init__(self):
-        self.capacity = capacity
+        self.capacity = 10000
         self.memory = []
         self.position = 0
     
@@ -65,138 +65,13 @@ class ReplayMemory:
         self.position = (self.position + 1) % self.capacity
         
     def sample(self):
-        return random.sample(self.memory, batch_size)
+        return random.sample(self.memory, 64)
 
     def __len__(self):
         return len(self.memory)
 ###############################################
 # End of Additional Template Codes            #
 ###############################################
-
-class Pacman(PacmanUtils):
-    def __init__(self):
-        print("Started just Pacman")
-        print("just sample")
-
-        self.step = 10000
-        self.model = None
-
-        self.epsilon = EPSILON_START
-        self.win_counter = 0  # number of victory episodes
-        self.steps_taken = 0  # steps taken across episodes
-        self.steps_per_epi = 0  # steps taken in one episodes
-        self.episode_number = 0
-        self.episode_rewards = []
-
-        self.policy = np.zeros()
-
-        def predict(self, state):
-            # state를 넣어 policy에 따라 action을 반환 (epsilon greedy)
-            # Hint: network에 state를 input으로 넣기 전에 preprocessing 해야합니다.
-            # print(state)
-            if np.random.random()<self.epsilon:
-                self.action = self.policy()
-
-            act = np.random.randint(0, 4)  # random value between 0 and 3
-
-            self.action = act  # save action
-            return act
-
-        def update_epsilon(self):
-            sample = random.random()
-            self.epsilon = EPSILON_END + (EPSILON_START - EPSILON_END) * \
-                            math.exp(-1. * self.steps_taken / 200)
-            self.steps_taken += 1
-
-
-        def step(self, next_state, reward, done):
-            # next_state = self.state에 self.action 을 적용하여 나온 state
-            # reward = self.state에 self.action을 적용하여 얻어낸 점수.
-            if self.action is None:
-                self.state = self.preprocess(next_state)
-            else:
-                self.next_state = self.preprocess(next_state)
-
-                self.state = self.next_state
-
-            # next
-            self.episode_reward += reward
-            self.steps_taken += 1
-            self.steps_per_epi += 1
-            self.update_epsilon()
-
-            if (self.trained_model == False):
-                self.train()
-                self.update_epsilon()
-                if (self.steps_taken % TARGET_UPDATE_ITER == 0):
-                    # UPDATE target network
-                    pass
-
-        def train(self):
-            # replay_memory로부터 mini batch를 받아 policy를 업데이트
-            if (self.steps_taken > LEARNING_STARTS):
-                transitions = memory.sample(BATCH_SIZE)
-                batch = Transition(*zip(*transitions))
-
-            else
-                return
-
-        def reset(self):
-            # 새로운 episode 시작시 불리는 함수.
-            self.last_score = 0
-            self.current_score = 0
-            self.episode_reward = 0
-
-            self.episode_number += 1
-            self.steps_per_epi = 0
-
-        def final(self, state):
-            # episode 종료시 불리는 함수.
-            done = True
-            reward = self.getScore(state)
-            if reward >= 0:  # not eaten by ghost when the game ends
-                self.win_counter += 1
-
-            self.step(state, reward, done)
-            self.episode_rewards.append(self.episode_reward)
-            win_rate = float(self.win_counter) / 500.0
-            avg_reward = np.mean(np.array(self.episode_rewards))
-            # print episode information
-            if (self.episode_number % 500 == 0):
-                print(
-                    "Episode no = {:>5}; Win rate {:>5}/500 ({:.2f}); average reward = {:.2f}; epsilon = {:.2f}".format(
-                        self.episode_number,
-                        self.win_counter, win_rate, avg_reward, self.epsilon))
-                self.win_counter = 0
-                self.episode_rewards = []
-                if (self.trained_model == False and self.episode_number % 1000 == 0):
-                    # Save model here
-                    # torch.save(self.YOUR_NETWORK, self.model)
-                    pass
-
-        def blank_grid(self, state, N):
-            width, height = state.data.layout.width, state.data.layout.height
-            grid = np.zeros((N, width, height))
-            return grid
-
-        def pacman_grid(self, state):
-            return state.getPacmanPosition()
-
-        def ghost_grid(self, state):
-            return state.getScaredTimer()
-
-        def preprocess(self, state):
-            # pacman.py의 Gamestate 클래스를 참조하여 state로부터 자유롭게 state를 preprocessing 해보세요.
-            pacman_state = state.getPacmanPosition()
-            ghost_state = state.getGhostPositions()[0]
-            food_state = state.getFood()
-            walls_state = state.getWalls()
-            capsule_state = state.getCapsules()
-            # reward          = state.getScore()
-            state = {"pacman": pacman_state, "ghost": ghost_state, "food": food_state, "wall": walls_state,
-                     "capsule": capsule_state}
-            print(state)
-            return state
 
 
 class PacmanDQN(PacmanUtils):
@@ -247,20 +122,13 @@ class PacmanDQN(PacmanUtils):
     
     
     def update_epsilon(self):
-        global steps_done
+
         sample = random.random()
-        eps_threshold = EPS_END + (EPS_START - EPS_END) * \
-                        math.exp(-1. * steps_done / EPS_DECAY)
-        steps_done += 1
-        if sample > eps_threshold:
-            with torch.no_grad():
-                # t.max (1)은 각 행의 가장 큰 열 값을 반환합니다.
-                # 최대 결과의 두번째 열은 최대 요소의 주소값이므로,
-                # 기대 보상이 더 큰 행동을 선택할 수 있습니다.
-                return policy_net(state).max(1)[1].view(1, 1)
-        else:
-            return torch.tensor([[random.randrange(n_actions)]], device = device, dtype = torch.long)
-    
+        eps_threshold = EPSILON_END + (EPSILON_START - EPSILON_END) * \
+                        math.exp(-1. * self.steps_taken / 200)
+        self.steps_taken += 1
+
+        return
     
     def step(self, next_state, reward, done):
         # next_state = self.state에 self.action 을 적용하여 나온 state
@@ -292,7 +160,7 @@ class PacmanDQN(PacmanUtils):
             transitions = memory.sample(BATCH_SIZE)
             batch = Transition(*zip(*transitions))
 
-        else
+        else:
             return
     
     
@@ -339,16 +207,60 @@ class PacmanDQN(PacmanUtils):
     def ghost_grid(self, state):
         return state.getScaredTimer()
 
+    def grid2array(self, blank_grid, index, method):
+        blank_grid[index] = [x[:] for x in method]
+        return blank_grid
+
+    def scalar2array(self, blank_grid, index, method, expected=None):
+        if expected is None:
+            expected = [0, 0]
+
+        pos = method
+
+        if index != 4:
+            x = blank_grid.shape[1]-1-pos[1]+expected[0]
+            y = pos[0] + expected[1]
+            blank_grid[index][int(x)][int(y)] = 1
+
+        if index == 4:
+            if pos != []:
+                x = blank_grid.shape[1]-1-pos[0][1]+expected[0]
+                y = pos[0][0]+expected[1]
+                blank_grid[index][int(x)][int(y)] = 1
+
+        return blank_grid
+
+    def state2array(self, blank_grid, index, method, state):
+        data = method.__str__()
+        data = data.split(", ")[2]
+        expected_pos = [0, 0]
+        if data == "West":
+            expected_pos = [-1, 0]
+        elif data == "East":
+            expected_pos = [1, 0]
+        elif data == "North":
+            expected_pos = [0, 1]
+        elif data == "South":
+            expected_pos = [0, -1]
+
+        if index == 5:
+            blank_grid = self.scalar2array(blank_grid, 5, state.getPacmanPosition(), expected=expected_pos)
+        elif index == 6:
+            blank_grid = self.scalar2array(blank_grid, 6, state.getGhostPositions()[0], expected=expected_pos)
+        else:
+            print("Something wrong here")
+        return blank_grid
+
     def preprocess(self, state):
         # pacman.py의 Gamestate 클래스를 참조하여 state로부터 자유롭게 state를 preprocessing 해보세요.
-        pacman_state    = state.getPacmanPosition()
-        ghost_state     = state.getGhostPositions()[0]
-        food_state      = state.getFood()
-        walls_state     = state.getWalls()
-        capsule_state   = state.getCapsules()
-        # reward          = state.getScore()
-        state = {"pacman" : pacman_state, "ghost": ghost_state,"food": food_state, "wall":walls_state, "capsule": capsule_state}
-        print(state)
-        return state
+        blank_grid = self.blank_grid(state, 7)
+        blank_grid  = self.scalar2array(blank_grid, 0, state.getPacmanPosition())
+        blank_grid  = self.scalar2array(blank_grid, 1, state.getGhostPositions()[0])
+        blank_grid  = self.grid2array(blank_grid, 2, state.getFood())
+        blank_grid  = self.grid2array(blank_grid, 3, state.getWalls())
+        blank_grid  = self.scalar2array(blank_grid, 4, state.getCapsules())
+        blank_grid  = self.state2array(blank_grid, 5, state.data.agentStates[0], state)
+        blank_grid  = self.state2array(blank_grid, 6, state.data.agentStates[1], state)
+        return blank_grid
 
 
