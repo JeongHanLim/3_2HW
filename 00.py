7df34783fe853688a36548257d11b88273a0c34f
JeongHanLim <jhlim271@naver.com>
2021-05-04 23:25:05 +0900

    adjustment.

--- 00.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-
-filename = "input_data.csv"
-df = pd.read_csv(filename)
-
-print(df)
--- test.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-int main()
-{
-    int x = 56;
-    char c = x;
-    write(1, &c, sizeof(c));
-
-
-}
